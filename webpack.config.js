bbd303d5814ea411db2fb988730d9a9d260c89bd
yTakkar <www.shtakkar@gmail.com>
2018-03-04 23:33:38 +0530

    Updated babel for it work well with latest version of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const path = require('path')
 module.exports = {
   entry: './public/js/src/main.js',
   output: {
+    // eslint-disable-next-line no-undef
     path: path.join(__dirname, '/public/js/dist/'),
     filename: 'bundle.js'
+  },
+  mode: 'development',
+  performance: {
+    hints: false,
   },
   module: {
     rules: [
